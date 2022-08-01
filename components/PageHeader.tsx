51ccb5970c7cdb522382f421dbed011dd7816fb3
mar-cial <cesarmarcialq@gmail.com>
2022-08-01 14:27:45 -0700

    metamask button working v1

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -2,8 +2,7 @@ import Link from 'next/link';
 
 const PageHeader = () => {
   return (
-    <header className="relative z-50 grid w-full grid-cols-3 px-2 py-4 text-center md:px-4">
-      <div></div>
+    <header className="relative z-50 flex justify-center w-full grid-cols-3 px-2 py-4 text-center md:px-4">
       <div>
         <Link href={'/'} passHref>
           <a className="text-xl font-bold text-white font-display">
@@ -11,9 +10,7 @@ const PageHeader = () => {
           </a>
         </Link>
       </div>
-      <div className="flex justify-end">
-        <button className="font-display">Connect to MetaMask</button>
-      </div>
+      
     </header>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,67 @@
-import { NextPage } from 'next'
-import { ethers } from 'ethers'
+import { NextPage } from 'next';
+import { ethers } from 'ethers';
 
 // component imports
-import Banner from '@components/Banner'
-import Footer from '@components/Footer'
-import FurnitureRibbon from '@components/FurnitureRibbon'
-import { useState } from 'react'
-
+import Banner from '@components/Banner';
+import Footer from '@components/Footer';
+import FurnitureRibbon from '@components/FurnitureRibbon';
+import { useEffect, useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 // component start
 const Homepage: NextPage = () => {
-  const [errorMessage, setErrorMessage] = useState<undefined|string>()
-  const [defaultAccount, setDefaultAccount] = useState<undefined|string>()
-  const [userBalance, setUserBalance] = useState<undefined|string>()
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [hasMetamask, setHasMetamask] = useState<boolean>(false);
+  const [signer, setSigner] = useState<any>();
 
+  useEffect(() => {
+    // @ts-ignore
+    if (window.ethereum !== 'undefined') {
+      setHasMetamask(true);
+    }
+  });
+
+  const connect = async () => {
+    // @ts-ignore
+    if (typeof window.ethereum !== 'undefined') {
+      try {
+        // @ts-ignore
+        await ethereum.request({ method: 'eth_requestAccounts' });
+        setIsConnected(true);
+        // @ts-ignore
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setSigner(provider.getSigner());
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      setIsConnected(false);
+    }
+  };
   return (
-    <div className='text-white bg-black'>
+    <div className="text-white bg-black">
+      <AnimatePresence>
+        {hasMetamask ? (
+          isConnected ? (
+            <motion.div animate={{ height: 0, transition: { delay: 1 } }}>
+              <button>Connected!</button>
+            </motion.div>
+          ) : isConnected ? (
+            ''
+          ) : (
+            <motion.div>
+              <button onClick={connect}>Connect to MetaMask</button>
+            </motion.div>
+          )
+        ) : (
+          'Please install MetaMask.'
+        )}
+      </AnimatePresence>
       <Banner />
       <FurnitureRibbon />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
