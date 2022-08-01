cdb6c35d884858978f458309f80e77d609631e34
mar-cial <cesarmarcialq@gmail.com>
2022-08-01 13:21:58 -0700

    added black transparent overlay to video div for text clarity

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -7,7 +7,7 @@ const Banner = () => {
     const ribbon = document.querySelector('#furniture-ribbon');
     ribbon?.scrollIntoView({ behavior: 'smooth', block: 'start' });
   };
-  
+
   return (
     <div className="relative min-h-screen">
       <video
@@ -20,19 +20,20 @@ const Banner = () => {
         Your browser does not support the video tag.
       </video>
 
-      <PageHeader />
+      <div className="absolute w-full h-full min-h-screen bg-black opacity-50"></div>
+      <main className="absolute z-50 flex flex-col items-center justify-between h-full pb-12 text-center">
+        <PageHeader />
 
-      <main className="relative z-50 flex flex-col items-center justify-center h-[80vh] px-6 py-12 text-center ">
-        <h2 className="text-7xl text-center font-display sm:w-[30vw] md:w-[40vw]">
+        <h2 className="w-full text-6xl md:w-3/4 lg:w-1/2 md:text-7xl font-display md:px-6 lg:px-12">
           Only the best furniture for your Metaverse property.
         </h2>
-      </main>
 
-      <div className="relative z-50 flex justify-center text-3xl">
-        <motion.div whileHover={{ y: -4 }} whileTap={{ y: 0 }}>
-          <BsChevronDoubleDown onClick={scrollToFurniture} />
-        </motion.div>
-      </div>
+        <div className="z-50 flex justify-center text-3xl ">
+          <motion.div whileHover={{ y: -4 }} whileTap={{ y: 0 }}>
+            <BsChevronDoubleDown onClick={scrollToFurniture} />
+          </motion.div>
+        </div>
+      </main>
     </div>
   );
 };
