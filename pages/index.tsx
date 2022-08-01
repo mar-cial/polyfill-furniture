89822d72b0b90f9ad19e2e8667e29bce0f8a1db8
mar-cial <cesarmarcialq@gmail.com>
2022-08-01 13:24:18 -0700

    added ethers.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import { NextPage } from 'next'
-import React from 'react'
-import Banner from '@components/Banner'
-import FurnitureRibbon from '@components/FurnitureRibbon'
-import Footer from '@components/Footer'
+import { ethers } from 'ethers'
 
+// component imports
+import Banner from '@components/Banner'
+import Footer from '@components/Footer'
+import FurnitureRibbon from '@components/FurnitureRibbon'
+import { useState } from 'react'
+
+// component start
 const Homepage: NextPage = () => {
+  const [errorMessage, setErrorMessage] = useState<undefined|string>()
+  const [defaultAccount, setDefaultAccount] = useState<undefined|string>()
+  const [userBalance, setUserBalance] = useState<undefined|string>()
+
   return (
     <div className='text-white bg-black'>
       <Banner />
