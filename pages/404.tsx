0fdda5a4fe5d219c36c2fc791b4ea6e598cde52e
mar-cial <cesarmarcialq@gmail.com>
2022-08-01 10:03:36 -0700

    created and improved styling of 404 not found page

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const NotFound = () => {
+  return (
+    <div className={'flex h-screen justify-center items-center p-12 flex-col gap-6 bg-black text-white text-center'}>
+      <h1 className="text-5xl font-display">NOT FOUND!</h1>
+      <Link href={'/'} passHref>
+        <a className='px-3 py-1 border font-text border-zinc-800'>Go back</a>
+      </Link>
+    </div>
+  )
+}
+
+export default NotFound
