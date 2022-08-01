5483f7d4344b604ed0c1d3e38ed43a897fa01176
mar-cial <cesarmarcialq@gmail.com>
2022-08-01 13:23:32 -0700

    added NextPage type to NotFound page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,7 @@
+import { NextPage } from 'next'
 import Link from 'next/link'
 
-const NotFound = () => {
+const NotFound: NextPage = () => {
   return (
     <div className={'flex h-screen justify-center items-center p-12 flex-col gap-6 bg-black text-white text-center'}>
       <h1 className="text-5xl font-display">NOT FOUND!</h1>
